e6d04eb0730eb0b62d11aa293e8b3e46050ad0d0
Alister <alister.jupp@gmail.com>
2015-05-22 21:37:18 +0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+adjectives = Adjective.create([{adjective:'dog'}])
+nouns = Noun.create([{noun:'angry'}])

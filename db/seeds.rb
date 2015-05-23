436dac0752ffb9a9f33ba4b3ff28882f4272485e
Alister <alister.jupp@gmail.com>
2015-05-23 17:37:23 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-adjectives = Adjective.create([{adjective:'angry'}])
-nouns = Noun.create([{noun:'dog'}])
+adjectives = Adjective.create([{adjective:'angry'},{adjective:'Lost'},{adjective:'Drunken'},{adjective:'Retarded'},{adjective:'speeding'}, 	
+{adjective:'tasty'},{adjective:'sour'}, {adjective:'zany'}, 		
+{adjective:'naked'},{adjective:'neutered'}, {adjective:'neurotic'}, {adjective:'xenophobic'}, {adjective:'queasy'}, {adjective:'jihadi'}, {adjective:'jealous'}, 	
+{adjective:'raging'},{adjective:'jesting'}, {adjective:'questionable'},{adjective:'kooky'}, {adjective:'retroactive'},{adjective:'flaccid'},{adjective:'funky'}])
+nouns = Noun.create([{noun:'dog'}, {noun:'Cat'},{noun:'Fish'},{noun:'Muppet'},{noun:'bottle'},{noun:'godzilla'},
+{noun:'courage'},{noun:'faculty'},{noun:'pollution'},{noun:'squirrel'},{noun:'territory'},{noun:'temper'},{noun:'volcano'},{noun:'earthquake'},
+{noun:'amusement'},{noun:'apparatus'},{noun:'hammer'},{noun:'history'},{noun:'hydrant'},{noun:'humour'},{noun:'rabbit'}])
